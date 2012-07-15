ecf5e8c3ac4496d2ba89dc44b431d31227838a62
monkeybad <rocker.roller.justin.lee@gmail.com>
2012-07-16 00:56:45 +0800

    add yungujia

---
+++ yungujia/yungujia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yungujia
+//
+//  Created by lijinxin on 12-7-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
